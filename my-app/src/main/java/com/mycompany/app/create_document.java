953c6ed811268c9a230f06f482fbc18d274acf5f
Stuart Smith <s.smith.5@elsevier.com>
2021-02-01 14:54:03 +0000

    initial commit

---
+++ my-app/src/main/java/com/mycompany/app/create_document.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+import com.aspose.words.Document;
+import com.aspose.words.Paragraph;
+import com.aspose.words.Section;
+import com.aspose.words.DocumentBuilder;
+//import com.aspose.words.utils.*;
+import java.io.File;
+
+
+public class create_document 
+{
+    public static void main( String[] args) throws Exception
+    {
+        
+        String dataDir = Utils.getDataDir(App.class);
+        // Open the document.
+        Document doc = new Document();
+        DocumentBuilder builder = new DocumentBuilder(doc);
+        builder.writeln("Enter Text for the new document");
+        doc.save(dataDir + "Name the document.docx");
+        System.out.println("New Word document created successfully.");
+   
+        
+    }
+}
